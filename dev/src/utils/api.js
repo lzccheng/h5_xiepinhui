6ecf16e4f80532fd3706313711be06e462221009
“librap45” <1131170034@qq.com>
2018-10-19 10:25:00 +0800

    10/19

--- dev/src/utils/api.js
+++ dev/src/utils/api.js
@@ -2,9 +2,11 @@ import * as user from './api_modules/user'
 import * as center from './api_modules/center'
 import * as wx from './api_modules/wx'
 import * as comm from './api_modules/comm'
+import * as myshop from './api_modules/myshop'
 export const api = {
   ...user,
   ...center,
   ...wx,
-  ...comm
+  ...comm,
+  ...myshop
 }
---
+++ dev/src/utils/api_modules/myshop.js
@@ -0,0 +1,56 @@
+import {
+  domain
+} from '../domain.js'
+import {
+  GET,
+  POST,
+  PUT,
+  DELETE
+} from "../request"
+
+//子账号列表接口
+export const allMyShopList = (data) => {
+  const url = `${domain}/restore/sublist`
+  return POST(url, data)
+}
+//添加子账号接口
+export const addSubStore = (data) => {
+  const url = `${domain}/restore/addsub`
+  return POST(url, data)
+}
+//重新密码接口
+export const resetManagePwd = (data) => {
+  const url = `${domain}/restore/setsubpwd`
+  return POST(url, data)
+}
+//线下店铺列表接口
+export const offlineStoreList = (data) => {
+  const url = `${domain}/restore/subsaleslist`
+  return POST(url, data)
+}
+//线下店铺订单列表接口
+export const offlineStoreOrderList = (data) => {
+  const url = `${domain}/restore/suborder`
+  return POST(url, data)
+}
+//总店铺粉丝管理接口
+export const substorefanManage = (data) => {
+  const url = `${domain}/restore/substorefan`
+  return POST(url, data)
+}
+//总店铺子账号粉丝列表接口
+export const substorefanList = (data) => {
+  const url = `${domain}/restore/subfan`
+  return POST(url, data)
+}
+//订单列表数量接口
+// export const newgetorderlist_num = (data) => {
+//   const url = `${domain}/ninegroup/newgetorderlist_num`
+//   return POST(url, data)
+// }
+
+// //个人中心首页接口
+// export const newredmessage = (data) => {
+//   const url = `${domain}/nine/newredmessage`
+//   return POST(url, data)
+// }
